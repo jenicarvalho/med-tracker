c1e7547ae3eb11b951a947249992526cffe2d709
Jeniffer Carvalho <jenicarvalho@live.com>
2026-06-27 16:05:16 -0300

    add under construction page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Under construction</h1>
+      <p>This is the main landing page of our application.</p>
+    </div>
+  );
+}
+
+export default Home;
